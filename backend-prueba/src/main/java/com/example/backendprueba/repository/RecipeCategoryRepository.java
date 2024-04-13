4eaea0b0e5c2d90ebd02de4effeb8671205d4d07
Lizzie6666 <111586732+Lizzie6666@users.noreply.github.com>
2024-04-13 11:40:41 -0500

    Add files via upload

---
+++ backend-prueba/src/main/java/com/example/backendprueba/repository/RecipeCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.foodia.pruebafoodia.repository;
+import com.foodia.pruebafoodia.entities.RecipeCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeCategoryRepository extends JpaRepository<RecipeCategory, Long> {
+
+}
