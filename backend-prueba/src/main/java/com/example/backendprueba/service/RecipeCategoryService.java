04703fa4781e1c03ead8986f1fab09ac77c89320
Lizzie6666 <111586732+Lizzie6666@users.noreply.github.com>
2024-04-13 11:42:34 -0500

    Recipe Category Service

---
+++ backend-prueba/src/main/java/com/example/backendprueba/service/RecipeCategoryService.java
@@ -0,0 +1,33 @@
+package com.foodia.pruebafoodia.service;
+
+import com.foodia.pruebafoodia.entities.RecipeCategory;
+import com.foodia.pruebafoodia.repository.RecipeCategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+public class RecipeCategoryService {
+    private final RecipeCategoryRepository recipeCategoryRepository;
+
+    @Transactional
+    public RecipeCategory register(RecipeCategory recipeCategory){
+        return recipeCategoryRepository.save(recipeCategory);
+    }
+    public List<RecipeCategory> listado(){
+        return recipeCategoryRepository.findAll();
+    }
+
+    public RecipeCategory update(RecipeCategory recipeCategory) throws Exception {
+        recipeCategoryRepository.findById(recipeCategory.getId()).orElseThrow(() -> new Exception("No se encontró entidad"));
+        return recipeCategoryRepository.save(recipeCategory);
+    }
+
+    public RecipeCategory delete(Long codigo) throws Exception{
+        RecipeCategory recipeCategory = recipeCategoryRepository.findById(codigo).orElseThrow(() -> new Exception("No se encontró entidad"));
+        recipeCategoryRepository.delete(recipeCategory);
+        return recipeCategory;
+    }
+}
